ec68dc7d86e259242676a61aac34cce8e8d89d00
zero2018 <76695802+zeroIQ2018@users.noreply.github.com>
2023-01-22 21:40:41 +0100

    script repair

--- Assets/Scripts/SelectUnit.cs
+++ Assets/Scripts/SelectUnit.cs
@@ -6,14 +6,14 @@ using static UnityEngine.GraphicsBuffer;
 
 public class SelectUnit : MonoBehaviour
 {
-    int selectedx;
-    int selectedy;
     bool selected = false;
     public Camera camera;
     float ys;
     float xs;
     float x;
     float y;
+    float bxs;
+    float bys;
     int result;
     public GameObject _inf;
     string[,] GameArray = Scenario1.myArray;
@@ -23,12 +23,12 @@ public class SelectUnit : MonoBehaviour
         camera = Camera.main;
     }
 
-    void DestroyWithTag (string destroyTag)
+    void DestroyWithTag(string destroyTag)
     {
         GameObject[] destroyObject;
         destroyObject = GameObject.FindGameObjectsWithTag(destroyTag);
         foreach (GameObject oneObject in destroyObject)
-        Destroy (oneObject);
+            Destroy(oneObject);
     }
 
 
@@ -37,71 +37,69 @@ public class SelectUnit : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {   
+        {
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit))
-            {   
+            {
                 if (hit.collider != null)
                 {
-                    Debug.Log($"{hit.collider.name} Detected",hit.collider.gameObject);
-                    if(selected == true)
+                    Debug.Log($"{hit.collider.name} Detected", hit.collider.gameObject);
+                    if (selected == true)
                     {
-                        
+
                         x = hit.collider.transform.position.x;
                         y = hit.collider.transform.position.y;
                         if (GameArray[(int)x, (int)y] == "E")
                         {
-                            if (Mathf.Abs(xs - x) <= 1 && Mathf.Abs(ys - y) <= 1){
+                            if (Mathf.Abs(xs - x) <= 1 && Mathf.Abs(ys - y) <= 1)
+                            {
                                 GameArray[(int)xs, (int)ys] = "E";
                                 GameArray[(int)x, (int)y] = "I";
                                 DestroyWithTag(("FieldObject"));
                                 CreateGame();
                                 selected = false;
-                                
+
                             }
                         }
 
 
-                    
-                    
+
+
                     }
-                if(GameArray[(int)xs, (int)ys] != "E" && selected == false)
-                
-                {
-                    xs = hit.collider.transform.position.x;
-                    ys = hit.collider.transform.position.y;
-                    selected = true;
-                }
-                
-                
-                }
-                else{
-                    selected = false;
-                    
+                    if (selected == false)
+                    {
+                        bxs = hit.collider.transform.position.x;
+                        bys = hit.collider.transform.position.y;
+                        if (GameArray[(int)bxs, (int)bys] != "E")
+                        {
+                            xs = hit.collider.transform.position.x;
+                            ys = hit.collider.transform.position.y;
+                            selected = true;
+                        }
 
-                    
+
+                    }
+                    Debug.Log(selected);
                 }
-                Debug.Log(selected);
             }
+
         }
 
-    }
 
-    
-    void CreateGame()
-    {
-        for (int i = 0; i < GameArray.GetLength(0); i++)
+        void CreateGame()
         {
-            for (int j = 0; j < GameArray.GetLength(1); j++)
+            for (int i = 0; i < GameArray.GetLength(0); i++)
             {
-                if (GameArray[i, j] == "I")
+                for (int j = 0; j < GameArray.GetLength(1); j++)
                 {
-                    Instantiate( _inf, new Vector3(i, j, -0.1f), Quaternion.identity);
+                    if (GameArray[i, j] == "I")
+                    {
+                        Instantiate(_inf, new Vector3(i, j, -0.1f), Quaternion.identity);
+                    }
                 }
             }
         }
     }
-
 }
